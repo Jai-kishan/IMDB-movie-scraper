352ff3bb6ba730017f2328068eb2e4862a757410
Jai Dev <35428869+Jai-kishan@users.noreply.github.com>
2019-04-27 15:52:40 +0530

    update IMDB_task_2
    Task 1 mein scrape_top_list() function likh ke saari movies ki list nikaal thi . Iss task mein humein group_by_year naam ka ek naya function likhna hai jo ki movies ko by year group karega.

---
+++ web_scraping/IMDB_task_2.py
@@ -0,0 +1,90 @@
+from urllib.request import urlopen
+# Beautiful Soup is a Python library for pulling data out of HTML and XML files.
+from bs4 import BeautifulSoup
+#pprint — Data pretty printer. 
+#The pprint module provides a capability to “pretty-print” arbitrary Python data structures in a form which can be used as input to the interpreter.
+from pprint import pprint
+import os.path ##The OS module in python provides functions for interacting with the operating system.
+
+#The JSON module is mainly used to convert the python dictionary above into a JSON string that can be written into a file.
+#While the JSON module will convert strings to Python datatypes,
+#normally the JSON functions are used to read and write directly from JSON files
+import json
+
+def scrape_top_list():
+	if os.path.exists("Top_250_movies.json"):
+		with open("Top_250_movies.json") as file:
+			read_file=file.read()
+			file_store=json.loads(read_file)
+		return file_store
+
+	imdb_url=urlopen("https://www.imdb.com/india/top-rated-indian-movies/")
+	soup=BeautifulSoup(imdb_url, "lxml")
+	main_div=soup.find("tbody", class_="lister-list")
+	table_row=main_div.find_all("tr")
+
+	movie_detail=[]
+	for tr in table_row:
+
+		#for movie position 
+		position=tr.find("td",class_="titleColumn").get_text().strip().split()
+		position=int(position[0].strip("."))
+
+		#Movie Name
+		movie_name=tr.find("td",class_="titleColumn").a.get_text()
+
+		#Movie Year
+		movie_year=tr.find("td",class_="titleColumn").span.get_text()
+		movie_year=movie_year.replace("(","").replace(")","")
+
+		#Movie Rating
+		rating=tr.find("td",class_="imdbRating").strong.get_text()
+
+		#Movie URL
+		url="https://www.imdb.com"
+		make_url=tr.find("td",class_="titleColumn").a["href"]
+		for i in make_url:
+			if "?" in i:
+				break
+			else:
+				url+=i
+		
+		store={
+		"position":position,
+		"Movie Name":movie_name,
+		"Movie Year":int(movie_year),
+		"Rating":float(rating),
+		"URL":url
+		}
+		
+		movie_detail.append(store)
+
+	# return(movie_detail)
+	with open("Top_250_movies.json","w") as file:
+		json.dump(movie_detail,file,indent=4)
+	return movie_detail
+
+
+movies=scrape_top_list()
+# pprint(movies)
+
+
+#Task 2
+def group_by_year(movies):
+	lst_of_year=[]
+	yearly_movies={}
+	for i in movies:
+		if i["Movie Year"] not in lst_of_year:
+			lst_of_year.append(i["Movie Year"])
+
+	#this mehthos use for sort the list element reverse
+	lst_of_year.sort(reverse=True)
+	print(lst_of_year)
+	for j in lst_of_year:
+		same_year=[]
+		for k in movies:
+			if j==k["Movie Year"]:
+				same_year.append(k)
+		yearly_movies[j]=same_year
+	return yearly_movies
+pprint(group_by_year(movies))
